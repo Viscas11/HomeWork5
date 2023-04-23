30165306011a237ed067c130fed9052ff2f20a1a
Syat <titmouse99@mail.ru>
2023-04-23 23:04:38 +0300

    5 задача

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -29,6 +30,12 @@ public class Main {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(" интерация цикла " + i);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является висыокосным ");
         }
     }
 }
