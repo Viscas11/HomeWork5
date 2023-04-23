0ef50f97abae5bf396b59f515ffe33b03b1f4ff3
Syat <titmouse99@mail.ru>
2023-04-23 22:54:01 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
+        System.out.println("Задача 1");
         for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(" интерация цикла " + i);
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i = i - 1) {
             System.out.println(" интерация цикла " + i);
         }
     }
