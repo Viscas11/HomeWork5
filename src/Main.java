0ebf662b3a81bf672240c06e68ef92d0a58d9fe2
Syat <titmouse99@mail.ru>
2023-04-23 23:10:43 +0300

    7 задача

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -42,6 +43,12 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(" интерация цикла " + i);
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(" интерация цикла " + i);
         }
     }
